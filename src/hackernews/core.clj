36962a374bc093027004a381437b60bf37d91d4a
syhw <gabriel.synnaeve@gmail.com>
2012-11-02 12:07:07 +0100

    tested writing down articles content

--- src/hackernews/core.clj
+++ src/hackernews/core.clj
@@ -1,16 +1,14 @@
 (ns hackernews.core
   (:require [clojure.java.io :refer :all]
+            [clojure.string :as s]
             [clj-http.client :as http]
             [tika])
   (:import [java.net URLEncoder])
   (:use [cheshire.core])
   (:gen-class))
 
-;(def filename "TEST.html")
-;(def filename "TEST2.html")
-;(def filename "TEST3.pdf")
-
 (def hn-url "http://api.thriftdb.com/api.hnsearch.com/items/_search?")
+(def folder-prefix "data/")
 
 (defn encode-url 
   [url & params]
@@ -20,27 +18,34 @@
          "&filter[field][type][]=submission&start=" (URLEncoder/encode 
                                                       (str (get kwp :start 0)))
          "&limit=" (URLEncoder/encode (str (get kwp :limit 100))))))
-; TEST (prn (:url (:item (first (:results (parse-string (:body (http/get (format (encode-url hn-url :start 0 :limit 100)))) true)))))))
-; TEST (prn (:text (tika/parse "dataset_army_composition.pdf"))) doesn't work, pdf can't be read TODO ?
+
+(defn clean-text
+  " Removes \n \t and html fields that passed through tika (#{header}...) "
+  [text]
+  (s/replace (s/replace 
+               (s/replace text #"\n" " ") #"\t" " ") #"#\{[^}]*\}" " "))
 
 (defn extract-article
+  " Calls tika, hopefully with the right MIME (from headers), on the url "
   [article-json]
-  (:text (tika/parse (:body (http/get (:url (:item article-json)))))))
+  (let [url (:url (:item article-json))]
+    {:url url, :text (clean-text (:text (tika/parse url)))}))
+
+(defn write-down 
+  [article]
+  (with-open [wrtr (writer (str folder-prefix (hash (:url article)) ".txt"))]
+    (.write wrtr (str (:url article) "\n"))
+    (.write wrtr (:text article))))
+
+; TEST (prn (:url (:item (first (:results (parse-string (:body (http/get (format (encode-url hn-url :start 0 :limit 100)))) true)))))))
+; TEST (prn (:text (tika/parse "test/dataset_army_composition.pdf"))) doesn't work, pdf can't be read TODO ?
+; TEST (write-down (extract-article (first (:results (parse-string (:body (http/get (format (encode-url hn-url :start 0 :limit 100)))) true)))))
 
 (defn -main [& args]
-;  (loop [start 0]
-;    DO SOMETHING
-;    (if (< start 900)
-;      (recur (+ start 100))
-;      ()))
- ; (map extract-article (:results (parse-string (:body (http/get 
- ;                 (format (encode-url hn-url :start 0 :limit 100)))) true))))
-  ;(prn (:text (tika/parse (:body (http/get "http://en.wikipedia.org/wiki/Mercury-Redstone_1"))))))
-  (keys (http/get "http://emotion.inrialpes.fr/people/synnaeve/index_files/dataset_army_composition.pdf"))
-  (:headers (http/get "http://emotion.inrialpes.fr/people/synnaeve/index_files/dataset_army_composition.pdf"))
-  (:text (tika/parse "TEST3.pdf"))
-  (type (:body (http/get "http://emotion.inrialpes.fr/people/synnaeve/index_files/dataset_army_composition.pdf")))
-  (prn (:text (tika/parse "http://emotion.inrialpes.fr/people/synnaeve/index_files/dataset_army_composition.pdf")))
-  )
-  ;(prn (:text (tika/parse (as-file (:body (http/get "http://emotion.inrialpes.fr/people/synnaeve/index_files/dataset_army_composition.pdf")))))))
+  (loop [start 0]
+    (map write-down (map extract-article (:results (parse-string (:body 
+        (http/get (format (encode-url hn-url :start 0 :limit 100)))) true)))))
+    (if (< start 900)
+      (recur (+ start 100))
+      ()))
 
