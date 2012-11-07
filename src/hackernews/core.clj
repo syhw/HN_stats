eaed705e75b2c3a7b2027176af38d7e6128043f9
syhw <gabriel.synnaeve@gmail.com>
2012-11-07 18:22:09 +0100

    corrects a bug and adds the textonly option to write-down

--- src/hackernews/core.clj
+++ src/hackernews/core.clj
@@ -10,6 +10,7 @@
 
 (def hn-url "http://api.thriftdb.com/api.hnsearch.com/items/_search?")
 (def folder-prefix "data/")
+(def textonly true)
 
 (defn encode-url 
   [url & params]
@@ -36,22 +37,23 @@
   [article-json]
   (let [url (:url (:item article-json)),
         domain (:domain (:item article-json))]
-    (if (= url nil)
+    (if (= nil url)
       {:url (str "http://news.ycombinator.com/item?id=" 
                  (first (s/split (:_id (:item article-json)) #"-"))),
        :domain domain,
        :text (remove-markup (:text (:item article-json)))}
       {:url url, 
        :domain domain,
-       :text (clean-text (:text tika/parse url))})))
+       :text (clean-text (:text (tika/parse url)))})))
       ;{:url url, :text (clean-text (:text (try (tika/parse url) 
       ;                                     (catch Exception e (str "caught exception: " e)))))})))
 
-(defn write-down 
+(defn write-down
   [article]
   (with-open [wrtr (writer (str folder-prefix (:domain article)
                                 (hash (:url article)) ".txt"))]
-    (.write wrtr (str (:url article) "\n"))
+    (if (= false textonly)
+      (.write wrtr (str (:url article) "\n")))
     (.write wrtr (:text article))))
 
 ; TEST (prn (:url (:item (first (:results (parse-string (:body (http/get (format (encode-url hn-url :start 0 :limit 100)))) true)))))))
@@ -64,14 +66,19 @@
 ;(map extract-article (:results (parse-string (:body (http/get (format (encode-url hn-url :start 0 :limit 5)))) true)))
 ;(map write-down (map extract-article (:results (parse-string (:body (http/get (format (encode-url hn-url :start 0 :limit 5)))) true))))
 
-(defn -main [& args]
+(defn fetch-articles []
   (loop [start 0]
     (prn (str "pulling links from: " start " to " (+ start 100)))
     (let [articles (:results (parse-string (:body 
-        (http/get (format (encode-url hn-url :start start :limit 100)))) true))]
+       (http/get (format (encode-url hn-url :start start :limit 100)))) true))]
       (pmap (comp write-down extract-article) articles)
       (prn (str "processed: " (count articles) " articles")))
     (if (< start 900)
       (recur (+ start 100))
       ())))
+
+(defn -main [& args]
+  (do
+    (fetch-articles)
+    ))
 
