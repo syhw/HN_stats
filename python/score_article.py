a9518e9e37485676a7aa5164854b6a5e31fa4b0d
syhw <gabriel.synnaeve@gmail.com>
2012-11-29 17:01:36 -0800

    score article with a naive Bayes++ model

---
+++ python/score_article.py
@@ -0,0 +1,35 @@
+import pickle, sys
+from gensim import utils
+
+#article_to_score = '../data/paulgraham.com-startupideas.html.txt'
+#article_to_score = '../data/paulgraham.com-founder.html.txt'
+article_to_score = '../data/paulgraham.com-ycombinator.html.txt'
+text = open(article_to_score, 'r').read()
+
+LEMMATIZE = utils.HAS_PATTERN
+lda = None
+if LEMMATIZE:
+    f = open('/Users/gabrielsynnaeve/Dropbox/Public/hn_lemmatized.ldamodel', 'r')
+    lda = pickle.load(f)
+    a = utils.lemmatize(text)
+else:
+    print >> sys.stderr, "ERROR: install pattern"
+    sys.exit(-1)
+
+user = 'pg'
+if len(sys.argv) > 1:
+    user = sys.argv[1]
+
+user_params = None
+with open(user + '.params') as f:
+    user_params = pickle.load(f)
+
+# score \proto P(Like) 
+# P(Like) \propto \sum_{t \in Topics}[P(TopicsArticle)
+#                 * P(\lambda|t,TopicsArticle) * P(Topics|Like) * P(Like)]
+score = 0.0
+for topicid, proba in lda[lda.id2word.doc2bow(a)]:
+    score += proba * user_params[topicid]
+
+print score
+
