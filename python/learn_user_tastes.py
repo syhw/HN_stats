e406d679c1da56af175598ea2383e7cc5e6785f5
syhw <gabriel.synnaeve@gmail.com>
2012-12-05 17:00:47 +0100

    learning parameters for all possible users

--- python/learn_user_tastes.py
+++ python/learn_user_tastes.py
@@ -2,13 +2,12 @@ import pickle, numpy, sys
 from gensim import utils
 
 user = 'pg'
-if len(sys.argv) > 1:
+users = None
+if 3 > len(sys.argv) > 1:
     user = sys.argv[1]
-
-
-with open('../data/' + user + '/interesting_articles.txt') as stalk_f:
-    articles = filter(lambda x: x != '',
-            stalk_f.read().rstrip('\n').split(' '))
+elif len(sys.argv) > 2:
+    if sys.argv[1] == '-f':
+        users = open(sys.argv[2])
 
 LEMMATIZE = utils.HAS_PATTERN
 lda = None
@@ -20,32 +19,47 @@ else:
     print >> sys.stderr, "ERROR: install pattern"
     sys.exit(-1)
 
-tastes = numpy.array([0.0 for i in range(lda.num_topics)])
-total = 0.0
-having = 0
-not_having = 0
+def extract_user(user):
+    with open('../data/' + user + '/interesting_articles.txt') as stalk_f:
+        articles = filter(lambda x: x != '',
+                stalk_f.read().rstrip('\n').split(' '))
 
-for article in articles:
-    print article
-    try:
-        text = open('../data/' + article + '.txt').read()
-        having += 1
-    except IOError: # we don't have this article
-        not_having += 1
-        continue
-    if LEMMATIZE:
-        a = utils.lemmatize(text)
-    else:
-        print >> sys.stderr, "ERROR: install pattern"
-        sys.exit(-1)
-    for topicid, proba in lda[lda.id2word.doc2bow(a)]:
-        total += proba
-        tastes[topicid] += proba
+    tastes = numpy.array([0.0 for i in range(lda.num_topics)])
+    total = 0.0
+    having = 0
+    not_having = 0
 
-tastes /= total
+    for article in articles:
+        #print article
+        try:
+            text = open('../data/' + article + '.txt').read()
+            having += 1
+        except IOError: # we don't have this article
+            not_having += 1
+            continue
+        if LEMMATIZE:
+            a = utils.lemmatize(text)
+        else:
+            print >> sys.stderr, "ERROR: install pattern"
+            sys.exit(-1)
+        for topicid, proba in lda[lda.id2word.doc2bow(a)]:
+            total += proba
+            tastes[topicid] += proba
 
-of = open(user+'.params', 'w')
-pickle.dump(tastes.tolist(), of)
+    tastes /= total
 
-print "we had:", having, "and missed:", not_having, "->", having*100.0/(having+not_having), "%"
+    of = open(user+'.params', 'w')
+    pickle.dump(tastes.tolist(), of)
 
+    print "For user:", user, " we had:", having, "and missed:", not_having, "->", having*100.0/(having+not_having+0.000001), "%"
+
+if users == None:
+    extract_user(user)
+else:
+    for line in users:
+        user = line.strip().rstrip('\n')
+        try:
+            with open('../data/' + user + '/interesting_articles.txt') as test_f: pass
+        except IOError:
+            print >> sys.stderr, "We do not have user:", user
+        extract_user(user)
