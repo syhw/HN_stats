e80f6dc5e42517e9109b41e7186c06b53ad4e672
syhw <gabriel.synnaeve@gmail.com>
2012-12-06 17:50:59 +0100

    wiki corpus building parallelized as by gensim

--- python/wiki_and_hn.py
+++ python/wiki_and_hn.py
@@ -1,35 +1,45 @@
-import sys, bz2, glob, pickle
+import sys, bz2, glob, pickle, multiprocessing
 from gensim import utils, models
 from gensim.corpora.dictionary import Dictionary
 from gensim.corpora.textcorpus import TextCorpus
 from gensim.corpora.mmcorpus import MmCorpus
-from gensim.corpora.wikicorpus import filter_wiki
+from gensim.corpora import wikicorpus
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-NO_BELOW = 21 # no word used less than 42/2 times
-NO_ABOVE = 0.1 # no word which is in above 10% of the corpus
+NO_BELOW = 32 # no word used less 32 times
+NO_ABOVE = 0.08 # no word which is in above 8% of the corpus
 VOCAB_SIZE = 200000 # 200k
-WIKI_ARTICLE_MIN_CHARS = 500
-LEMMATIZE = utils.HAS_PATTERN
+WIKI_ARTICLE_MIN_WORDS = 50
+HN_ARTICLE_MIN_WORDS = 42
 outputname = 'wiki_hn'
-if LEMMATIZE:
-    print "you have pattern: we will lemmatize ('you were'->'be/VB')"
-    outputname = outputname + "_lemmatized"
-else:
-    print "you don't have pattern: we will tokenize ('you were'->'you','were')"
 
 def tokenize(text):
     return [token.encode('utf8') for token in utils.tokenize(text, lower=True, errors='ignore') if 2 <= len(token) <= 20 and not token.startswith('_')]
 
+
 class WikiHNCorpus(TextCorpus):
-    def __init__(self, wiki_file, hn_folder, dictionary=None):
+    def __init__(self, wiki_file, hn_folder, dictionary=None, processes=None, 
+            lemmatize=utils.HAS_PATTERN):
         """
         Takes the wikipedia *articles.xml.bz2 and the HN folder of articles 
         as input and builds the dictionary and corpus
         """
+        global outputname
+        self.lemmatize = lemmatize
+        if self.lemmatize:
+            print "We will lemmatize ('you were'->'be/VB')"
+            self.outputname = outputname + "_lemmatized"
+        else:
+            print "We will only tokenize ('you were'->'you','were')"
+
         self.wiki_file = wiki_file
         self.hn_folder = hn_folder
+
+        if processes is None:
+            processes = max(1, multiprocessing.cpu_count() - 1)
+        self.processes = processes
+
         if dictionary is None:
             self.dictionary = Dictionary(self.get_texts())
             self.dictionary.filter_extremes(no_below=NO_BELOW, 
@@ -43,50 +53,40 @@ class WikiHNCorpus(TextCorpus):
         Iterate over the Wikipedia dump and the HN articles returning text
         """
         wiki_articles, hn_articles, articles_all = 0, 0, 0
-        # ************ Wikipedia ************
-        intext, positions = False, 0
-        for lineno, line in enumerate(bz2.BZ2File(self.wiki_file)):
-            if line.startswith('      <text'):
-                intext = True
-                line = line[line.find('>') + 1 : ]
-                lines = [line]
-            elif intext:
-                lines.append(line)
-            pos = line.find('</text>') # can be on the same line as <text>
-            if pos >= 0:
-                articles_all += 1
-                intext = False
-                if not lines:
-                    continue
-                lines[-1] = line[:pos]
-                text = filter_wiki(''.join(lines))
-                if len(text) > WIKI_ARTICLE_MIN_CHARS: # article redirects are pruned here
-                    wiki_articles += 1
-                    if LEMMATIZE:
-                        result = utils.lemmatize(text) # text into lemmas here
-                    else:
-                        result = tokenize(text) # text into tokens here
-                    positions += len(result)
-                    yield result
+        positions, positions_all = 0, 0
 
-        print (">>> finished iterating over Wikipedia corpus of %i documents with %i positions (total %i wiki articles before pruning)" % (wiki_articles, positions, articles_all))
-        # ************ /Wikipedia ************
+        # ************ Wikipedia ************
+        texts = ((text, self.lemmatize) for _, text in wikicorpus._extract_pages(bz2.BZ2File(self.wiki_file)))
+        pool = multiprocessing.Pool(self.processes)
+        for group in utils.chunkize(texts, chunksize=10 * pool._processes, maxsize=1): # otherwise imap puts all the corpus into memory
+            for tokens in pool.imap(wikicorpus.process_article, group):
+                articles_all += 1
+                positions_all += len(tokens)
+                if len(tokens) > WIKI_ARTICLE_MIN_WORDS:
+                    wiki_articles += 1
+                    positions += len(tokens)
+                    yield tokens
+        pool.terminate()
+
+        print (">>> finished iterating over Wikipedia corpus of %i documents with %i positions (total %i articles, %i positions before pruning articles shorter than %i words)" % (wiki_articles, positions, articles_all, positions_all, WIKI_ARTICLE_MIN_WORDS))
 
         # ************ HN articles ************
         positions_after_wiki = positions
         fnamelist = []
         for g in glob.iglob(self.hn_folder + '/*.txt'):
             fnamelist.append(g)
-        for fileno, fname in enumerate(fnamelist):
+        for fileno, fname in enumerate(fnamelist): # TODO parallelize as Wiki
             hn_text = open(fname).read()
-            articles_all += 1
-            hn_articles += 1
-            if LEMMATIZE:
+            if self.lemmatize:
                 result = utils.lemmatize(hn_text) # text into lemmas here
             else:
                 result = tokenize(hn_text) # text into tokens here
-            positions += len(result)
-            yield result
+            articles_all += 1
+            positions_all += len(result)
+            if len(result) > HN_ARTICLE_MIN_WORDS:
+                hn_articles += 1
+                positions += len(result)
+                yield result
 
         print (">>> finished iterating over HN corpus of %i documents with %i positions" % (hn_articles, positions - positions_after_wiki))
         # ************ /HN articles ************
